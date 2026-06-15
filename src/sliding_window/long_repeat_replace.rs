c122aa5096458ddfada04b30e811b4d22d64f88d
Ammar Abou Zor <a.abou.zor@reactive-dynamics.com>
2026-06-15 23:53:52 +0200

    Longest repeating character replacement

---
+++ src/sliding_window/long_repeat_replace.rs
@@ -0,0 +1,31 @@
+// Time O(N): Sliding window + O(26) in is_valid which is ignored as static
+// Space O(1) which is actually O(26).
+
+pub fn character_replacement(s: String, k: i32) -> i32 {
+    let mut char_counts = [0_i32; 26];
+
+    let mut left = 0;
+
+    let mut longest = 0;
+    let s = s.as_bytes();
+
+    for right in 0..s.len() {
+        char_counts[(s[right] - b'A') as usize] += 1;
+
+        while !is_valid(left, right, &char_counts, k) {
+            char_counts[(s[left] - b'A') as usize] -= 1;
+            left += 1;
+        }
+
+        longest = longest.max(right - left + 1);
+    }
+
+    longest as i32
+}
+
+fn is_valid(left: usize, right: usize, char_counts: &[i32], k: i32) -> bool {
+    let seq_len = right - left + 1;
+    let max_char = *char_counts.iter().max().unwrap();
+
+    seq_len as i32 - max_char <= k
+}
--- src/sliding_window/mod.rs
+++ src/sliding_window/mod.rs
@@ -1,2 +1,3 @@
 pub mod buy_sell_stocks;
+pub mod long_repeat_replace;
 pub mod long_substr_no_repeat;
