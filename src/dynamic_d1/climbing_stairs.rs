e5fa162078947faa2a065ea2863457ccb907cded
Ammar Abou Zor <a.abou.zor@reactive-dynamics.com>
2026-06-19 16:57:14 +0200

    DP: Climbing Stairs & Scaffolding

---
+++ src/dynamic_d1/climbing_stairs.rs
@@ -0,0 +1,45 @@
+// DP solution with optimization by avoiding saving the whole dp array as
+// we always need the last two numbers only.
+// We keep track on the last two numbers and keep moving.
+
+// Time O(n) we still visited each node
+// Space O(1) we need two variables only.
+
+pub fn climb_stairs_dp_optmized(n: i32) -> i32 {
+    let n = n as usize;
+
+    if n <= 2 {
+        return n as i32;
+    }
+
+    let mut prev1 = 2;
+    let mut prev2 = 1;
+    for _ in 3..=n {
+        let temp = prev1;
+        prev1 = prev1 + prev2;
+        prev2 = temp;
+    }
+
+    prev1
+}
+
+// Standard DP solution creating the DP vector and filling it from bottom up
+// Time: O(n) we visited each node once
+// Space: O(n) as we are creating the whole dp here.
+pub fn climb_stairs_dp(n: i32) -> i32 {
+    let n = n as usize;
+
+    if n <= 2 {
+        return n as i32;
+    }
+
+    let mut dp = vec![0; n + 1];
+
+    dp[1] = 1;
+    dp[2] = 2;
+    for i in 3..=n {
+        dp[i] = dp[i - 2] + dp[i - 1];
+    }
+
+    dp[n]
+}
